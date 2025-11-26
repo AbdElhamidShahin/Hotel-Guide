f7e8e11be98846f5502e382cb698820a9b588320
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2025-11-26 11:58:14 +0200

    -update some changed in notification screen

--- lib/features/notification/ui/notification_screen.dart
+++ lib/features/notification/ui/notification_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hotel_guide/core/router/routers.dart';
 import 'package:hotel_guide/features/notification/ui/widget/custom_item_notification.dart';
-
 import '../../../core/theme/app_theme.dart';
 import '../../../core/theme/colors.dart';
 
@@ -25,7 +26,9 @@ class NotificationScreenListView extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.arrow_forward, color: Colors.black),
-            onPressed: () {},
+            onPressed: () {
+              context.go(routes.homeScreen);
+            },
           ),
         ],
       ),
