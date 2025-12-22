5756688126cc6984938b1d146248c5dd36d4060c
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2025-12-22 22:21:20 +0200

    refactoring themes refactoring colors

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -1,45 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-
-
-  static const Color DarkPurpleGray = Color(0xFF51526C);
-
-  static const Color gray = Color(0xCCDDDDDD);
   static const Color gray2 = Color(0xFF939393);
   static const Color gray3 = Color(0xFF4A4A4A);
-  static const Color grayLight = Color(0x2BF3F3F3);
-  static const Color grayLight2 = Color(0xFF2F2F2);
   static const Color white = Color(0xFFFFFFFF);
   static const Color blueSoft = Color(0xFFCFDBEC);
   static const Color warningColor = Color(0xFFC8C8F4);
 
-  static const Color yellowSoft = Color(0xFFFFBD54);
   static const Color yellowGold = Color(0xFFFFC15F);
-  static const Color orangeLight = Color(0xFFD98E4E);
   static const Color orangeGold = Color(0xFFFFBD54);
-  static const Color orangeDark = Color(0xFFB25916);
 
-  static const Color brownDark = Color(0xFF181201);
   static const Color black = Color(0xFF000000);
   static const Color black2 = Color(0xFF343434);
-  static const Color black3 = Color(0xFF292929);
   static const Color black4 = Color(0xFF222222);
-  static const Color black45 = Color(0x80222222);
-  static const Color black5 = Color(0xFF1F1F1F);
   static const Color black6 = Color(0xFF171725);
   static const Color black7 = Color(0xFF101010);
-  static const Color black8 = Color(0xFF1F1F1F);
 
-
-
-  static const Color mainOrange = Color(0xFFFFBD59);     // لون الزر
-  static const Color borderGrey = Color(0xFFE0E0E0);     // لون الحدود
-  static const Color hintTextGrey = Color(0xFFD9D9D9);   // لون تلميح النص
-  static const Color textBlack = Color(0xFF1E1E1E);
-
-
-
+  static const Color mainOrange = Color(0xFFFFBD59);
+  static const Color borderGrey = Color(0xFFE0E0E0);
+  static const Color hintTextGrey = Color(0xFFD9D9D9);
 
   static const Color primary = Color(0xFF2D2B3E);
   static const Color colorText = Color(0xFF51526C);
@@ -54,9 +33,4 @@ class AppColors {
   static const Color red = Color(0xFFFF3838);
   static const Color redaccent = Color(0xFFD63B3B);
   static const Color LightRed = Color(0xFFFF7D86);
-
-
-
-
-
 }
