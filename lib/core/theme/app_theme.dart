6e76ac04742684c676bce275647735eeba66ef67
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2025-12-22 22:15:43 +0200

    Update Favorite Screem -added build NotFound Widget -added some images -added colors

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -128,7 +128,7 @@ TextStyle textStyle16BoldWhite = TextStyle(
   fontFamily: 'Cairo',
 
 );
-TextStyle textStyle12RegularShadowPurple = TextStyle(
+TextStyle textStyle18RegularShadowPurple = TextStyle(
   fontSize: 18,
   fontWeight: FontWeight.w400,
   color: AppColors.ShadowPurple,
