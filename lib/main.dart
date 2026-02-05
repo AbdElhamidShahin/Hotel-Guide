b2b08a82bdbbd6440c6e47b4d4cb4b9435fe90b0
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2026-02-05 13:31:36 +0200

    remove firebase in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'core/di/injection.dart';
-import 'firebase_options.dart';
 import 'hotel_app.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -17,6 +15,5 @@ void main() async {
         '***REMOVED***',
   );
   await initializeDateFormatting('ar_SA', null);
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(HotelApp());
 }
