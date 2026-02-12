c1a1099a4084e2cd885a62bba0ca69fc642bf628
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2026-02-12 11:55:17 +0200

    .

--- lib/features/profile/ui/profile_screen.dart
+++ lib/features/profile/ui/profile_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hotel_guide/core/router/routers.dart';
-import 'package:hotel_guide/features/on_boarding/ui/on_boarding_screen.dart';
 import 'package:hotel_guide/features/profile/ui/widget/build_sttings_item.dart';
 import 'package:hotel_guide/features/profile/ui/widget/custom_profile_image_and_name.dart';
 import '../../../core/helpers/local_storage_account.dart';
