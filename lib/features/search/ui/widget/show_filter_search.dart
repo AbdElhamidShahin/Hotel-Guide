b8dc79aebf2287374a5dc151729b65c876ba2cef
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2026-02-19 14:29:59 +0200

    Finshed Filter Search ui

--- lib/features/search/ui/widget/show_filter_search.dart
+++ lib/features/search/ui/widget/show_filter_search.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hotel_guide/core/theme/app_theme.dart';
 import 'package:hotel_guide/core/theme/colors.dart';
 import 'package:hotel_guide/features/search/ui/widget/price_range_filter.dart';
@@ -15,40 +16,55 @@ void showFilterSearch(
 }) {
   final searchCubit = context.read<SearchCubit>();
   FilterOptions tempFilter = FilterOptions();
+
   showModalBottomSheet(
     context: context,
     isScrollControlled: true,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
-    ),
+    backgroundColor: Colors.transparent,
     builder: (context) {
       return StatefulBuilder(
         builder: (context, setModalState) {
           return Container(
+            decoration: BoxDecoration(
+              color: AppColors.white,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
+            ),
             height: MediaQuery.of(context).size.height * 0.85,
-            padding: EdgeInsets.only(bottom: 20.h),
             child: Column(
               children: [
                 _buildFilterHeader(context),
 
                 Expanded(
                   child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
                     padding: EdgeInsets.symmetric(horizontal: 16.w),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         SizedBox(height: 20.h),
                         _buildSectionTitle("السعر"),
+                        SizedBox(height: 12.h),
+
                         Divider(
-                          height: 1,
+                          height: 1.h,
                           color: AppColors.primary.withOpacity(0.2),
                         ),
+
                         PriceRangeFilter(
                           initialValues: tempFilter.priceRange,
                           onChanged: (values) => tempFilter.priceRange = values,
                         ),
 
+                        SizedBox(height: 24.h),
                         _buildSectionTitle("المدينة"),
+                        SizedBox(height: 12.h),
+
+                        Divider(
+                          color: Colors.grey.withOpacity(0.3),
+                          height: 1.h,
+                        ),
+                        SizedBox(height: 16.h),
+
                         _buildChoiceChips(
                           items: cities,
                           selectedItem: tempFilter.city,
@@ -56,7 +72,16 @@ void showFilterSearch(
                               setModalState(() => tempFilter.city = val),
                         ),
 
+                        SizedBox(height: 24.h),
                         _buildSectionTitle("الإطلالة"),
+                        SizedBox(height: 12.h),
+
+                        Divider(
+                          color: Colors.grey.withOpacity(0.3),
+                          height: 1.h,
+                        ),
+                        SizedBox(height: 16.h),
+
                         _buildChoiceChips(
                           items: views,
                           selectedItem: tempFilter.view,
@@ -64,32 +89,63 @@ void showFilterSearch(
                               setModalState(() => tempFilter.view = val),
                         ),
 
+                        SizedBox(height: 24.h),
                         _buildSectionTitle("التقييم"),
+                        SizedBox(height: 12.h),
+
+                        Divider(
+                          color: Colors.grey.withOpacity(0.3),
+                          height: 1.h,
+                        ),
+                        SizedBox(height: 16.h),
+
                         _buildRatingChips(
                           selectedRating: tempFilter.rating,
                           onSelected: (val) =>
                               setModalState(() => tempFilter.rating = val),
                         ),
+
+                        SizedBox(height: 40.h),
                       ],
                     ),
                   ),
                 ),
 
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.w),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.primary,
-                      minimumSize: Size(double.infinity, 50.h),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.r),
+                  padding: EdgeInsets.only(bottom: 24.h, top: 12.h),
+                  child: Center(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height: 50.h,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.primary,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100.r),
+                          ),
+                        ),
+                        onPressed: () {
+                          searchCubit.applyFilter(tempFilter);
+                          Navigator.pop(context);
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("فلترة", style: textStyle16BoldWhite),
+                            SizedBox(width: 8.w),
+                            SvgPicture.asset(
+                              "assets/icons/Filter_alt.svg",
+                              colorFilter: const ColorFilter.mode(
+                                Colors.white,
+                                BlendMode.srcIn,
+                              ),
+                              width: 18.w,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    onPressed: () {
-                      searchCubit.applyFilter(tempFilter);
-                      Navigator.pop(context);
-                    },
-                    child: Text("تطبيق الفلتر", style: textStyle16BoldWhite),
                   ),
                 ),
               ],
@@ -102,9 +158,12 @@ void showFilterSearch(
 }
 
 Widget _buildSectionTitle(String title) {
-  return Padding(
-    padding: EdgeInsets.symmetric(vertical: 12.h),
-    child: Text(title, style: textStyle18BoldGray),
+  return Text(
+    title,
+    style: textStyle20BoldShadowPurple.copyWith(
+      color: AppColors.primary,
+      fontSize: 18.sp,
+    ),
   );
 }
 
@@ -115,29 +174,45 @@ Widget _buildChoiceChips({
 }) {
   return Directionality(
     textDirection: TextDirection.rtl,
-    child: Wrap(
-      spacing: 8.w,
-      children: items.map((item) {
-        bool isSelected = selectedItem == item;
-        return ChoiceChip(
-          label: Text(item),
-          selected: isSelected,
-          onSelected: (selected) => onSelected(item),
-          selectedColor: AppColors.primary,
-          labelStyle: TextStyle(
-            color: isSelected ? Colors.white : Colors.black,
-          ),
-          backgroundColor: Colors.grey[200],
-        );
-      }).toList(),
+    child: SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      physics: const BouncingScrollPhysics(),
+      child: Row(
+        children: items.map((item) {
+          bool isSelected = selectedItem == item;
+          return Padding(
+            padding: EdgeInsetsDirectional.only(end: 8.w),
+            child: ChoiceChip(
+              showCheckmark: false,
+              label: Text(
+                item,
+                style: textStyle18BoldGray.copyWith(
+                  color: isSelected ? Colors.white : AppColors.primary,
+                  fontSize: 14.sp,
+                ),
+              ),
+              selected: isSelected,
+              onSelected: (selected) => onSelected(item),
+              selectedColor: AppColors.primary,
+              backgroundColor: const Color(0xFFEBEBEB),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.r),
+                side: BorderSide(
+                  color: isSelected ? AppColors.primary : Colors.transparent,
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
     ),
   );
 }
 
 Widget _buildFilterHeader(BuildContext context) {
   return Container(
-    padding: EdgeInsets.symmetric(horizontal: 12.w),
-    height: 68.h,
+    padding: EdgeInsets.symmetric(horizontal: 8.w),
+    height: 60.h,
     decoration: BoxDecoration(
       color: AppColors.primary,
       borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
@@ -146,13 +221,15 @@ Widget _buildFilterHeader(BuildContext context) {
       children: [
         IconButton(
           onPressed: () => Navigator.pop(context),
-          icon: const Icon(Icons.close, color: Colors.white),
+          icon: Icon(Icons.close, color: Colors.white, size: 24.sp),
         ),
         const Spacer(),
         Text(
           "البحث المتقدم",
           style: textStyle16BoldWhite.copyWith(fontSize: 17.sp),
         ),
+        SizedBox(width: 40.w),
+        const Spacer(),
       ],
     ),
   );
@@ -162,28 +239,47 @@ Widget _buildRatingChips({
   double? selectedRating,
   required Function(double) onSelected,
 }) {
+  List<double> ratings = [1.0, 2.0, 3.0, 4.0, 5.0];
+
   return Directionality(
     textDirection: TextDirection.rtl,
-    child: Wrap(
-      spacing: 8.w,
-      children: [5.0, 4.0, 3.0, 2.0, 1.0].map((rate) {
-        bool isSelected = selectedRating == rate;
-        return ChoiceChip(
-          label: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text("$rate"),
-              const Icon(Icons.star, size: 14, color: Colors.amber),
-            ],
-          ),
-          selected: isSelected,
-          onSelected: (val) => onSelected(rate),
-          selectedColor: AppColors.primary,
-          labelStyle: TextStyle(
-            color: isSelected ? Colors.white : Colors.black,
-          ),
-        );
-      }).toList(),
+    child: SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      physics: const BouncingScrollPhysics(),
+      child: Row(
+        children: ratings.map((rate) {
+          bool isSelected = selectedRating == rate;
+          List<Widget> stars = [];
+          for (int i = 0; i < rate.toInt(); i++) {
+            stars.add(
+              SvgPicture.asset(
+                "assets/icons/rating/star-fill.svg",
+                width: 16.w,
+                height: 16.h,
+              ),
+            );
+          }
+
+          return Padding(
+            padding: EdgeInsetsDirectional.only(end: 8.w),
+            child: ChoiceChip(
+              showCheckmark: false,
+              padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
+              label: Row(mainAxisSize: MainAxisSize.min, children: stars),
+              selected: isSelected,
+              onSelected: (val) => onSelected(rate),
+              selectedColor: AppColors.primary,
+              backgroundColor: const Color(0xFFEBEBEB),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.r),
+                side: BorderSide(
+                  color: isSelected ? AppColors.primary : Colors.transparent,
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
     ),
   );
 }
