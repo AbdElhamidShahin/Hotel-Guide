67014415977d56ec7667e4a8c75804ab692ae04b
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2026-04-04 22:11:54 +0200

    d

--- lib/core/router/routers.dart
+++ lib/core/router/routers.dart
@@ -20,9 +20,6 @@ class routes {
   static const String BookingDetailsPage = "/BookingDetailsPage";
   static const String RoomDetailsPage = "/RoomDetailsPage";
   static const String FaqPage = "/FaqPage";
-<<<<<<< HEAD
   static const String AboutUsScreen = "/AboutUsScreen";
-=======
   static const String ChatScreen = "/ChatScreen";
->>>>>>> development-
 }
